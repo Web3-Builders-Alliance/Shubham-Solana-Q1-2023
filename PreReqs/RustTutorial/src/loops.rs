7c4da930bf43e03c88b17a0708d0cf576f0dd8b6
Shubham Sharma <94298264+Shubharma@users.noreply.github.com>
2023-02-05 23:57:51 +0530

    Add files via upload

---
+++ PreReqs/RustTutorial/src/loops.rs
@@ -0,0 +1,17 @@
+
+
+pub fn run() {
+  let mut count = 0;
+
+  for x in 0..100 {
+    if x % 15 == 0 {
+      println!("fizzbuzz");
+    } else if x % 3 == 0 {
+      println!("fizz");
+    } else if x % 5 == 0 {
+      println!("buzz")
+    } else {
+      println!("{}", x);
+    }
+  }
+}
